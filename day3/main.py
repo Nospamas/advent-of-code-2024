9a316093d4a6010728ea538f32e9672a653670b0
Nospamas <jnospamas@gmail.com>
2024-12-03 05:46:22 +0000

    steal timers from @AllanTaylor314

--- day3/main.py
+++ day3/main.py
@@ -1,6 +1,12 @@
 import re
+from time import perf_counter
+timer_script_start=perf_counter()
+
+timer_parse_start=perf_counter()
 file = open("day3/input.txt", "r")
 text = file.read()
+
+timer_parse_end=timer_part1_start=perf_counter()
 test = re.compile(r"mul\((\d+),(\d+)\)")
 
 allMatch = test.findall(text)
@@ -9,7 +15,7 @@ multiples = map(lambda x: int(x[0]) * int(x[1]), allMatch)
 total = sum(multiples)
 
 print(total)
-
+timer_part1_end=timer_part2_start=perf_counter()
 
 # split into do segements, anything at the beginning of these should be run
 doSegments = text.split("do()")
@@ -25,3 +31,10 @@ multiples2 = map(lambda x: int(x[0]) * int(x[1]), allMatch2)
 total2 = sum(multiples2)
 
 print(total2)
+timer_part2_end=timer_script_end=perf_counter()
+
+print(f"""Execution times (sec)
+Parse: {timer_parse_end-timer_parse_start:3.5f}
+Part1: {timer_part1_end-timer_part1_start:3.5f}
+Part2: {timer_part2_end-timer_part2_start:3.5f}
+Total: {timer_script_end-timer_script_start:3.5f}""")
